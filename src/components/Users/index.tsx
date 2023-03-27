29a02ec00c49f28b9f7279ea099919b24dee37d7
mohammed kabir <kbshowkb@gmail.com>
2023-03-27 21:05:46 +0100

    integrate filter

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
-import { UserContext, UserContextType } from "../../globalState";
+import { IUser, UserContext, UserContextType } from "../../globalState";
 import Cards from "./Cards";
 import Pagination from "./Pagination";
 import "./Users.scss";
-import UsersTable from "./UsersTable";
+import UsersTable, { FilterPayload } from "./UsersTable";
 
 const UsersContents = () => {
   const { loading, users, getUsers, usersOverview } = useContext(
     UserContext
   ) as UserContextType;
+
+  const [filterUsers, setfilterUsers] = useState<IUser[]>(users);
 
   useEffect(() => {
     if (!users.length) {
@@ -16,12 +18,16 @@ const UsersContents = () => {
     }
   }, []);
 
+  useEffect(() => {
+    setfilterUsers(users);
+  }, [users]);
+
   const [postPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
   const indexOfLastPage = currentPage * postPerPage;
   const indexOfFirstPage = indexOfLastPage - postPerPage;
-  const currentTemplate = users?.slice(indexOfFirstPage, indexOfLastPage);
-  const numberOfPages = Math.ceil(users?.length / postPerPage);
+  const currentTemplate = filterUsers?.slice(indexOfFirstPage, indexOfLastPage);
+  const numberOfPages = Math.ceil(filterUsers?.length / postPerPage);
 
   const previous = () => {
     if (currentPage > 1) {
@@ -38,11 +44,59 @@ const UsersContents = () => {
     }
   };
 
+  //  usernameRef: string | undefined;
+  //  email: string | undefined;
+  //  phone: number | undefined;
+  //  date: string | undefined;
+  //  org: string | undefined;
+  // status: string | undefined;
+
+  const filterSearch = (val: any) => {
+    console.log(val, "vallllll");
+    let filtered: any[] = [];
+    users.forEach((user) => {
+      if (user.status.toLowerCase().includes(val.status)) {
+        filtered.push(user);
+      }
+      // if (user.orgName.toLowerCase().includes(val.org)) {
+      //   console.log(user, "user");
+      //   filtered.push(user);
+      // }
+
+      // if (
+      //   user.userName.toLowerCase().includes(val?.username) ||
+      //   user.orgName.toLowerCase().includes(val.org) ||
+      //   user.email.toLowerCase().includes(val.email) ||
+      //   user.phoneNumber.toLowerCase().includes(val.phone) ||
+      //   user.orgName.toLowerCase().includes(val.org) ||
+      //   user.status.toLowerCase().includes(val.status)
+      // ) {
+      //   filtered.push(user);
+      //   filtered.push(user);
+      // }
+    });
+    console.log(filtered, "filteredss");
+    setfilterUsers(filtered);
+  };
+
+  const handleFilter = (val: any) => {
+    filterSearch(val);
+  };
+
+  const handleReset = () => {
+    setfilterUsers(users);
+  };
   return (
     <div className="users">
       <h3>Users</h3>
       <Cards usersOverview={usersOverview} loading={loading} />
-      <UsersTable users={currentTemplate} loading={loading} />
+      <UsersTable
+        filterUsers={currentTemplate}
+        loading={loading}
+        users={users}
+        handleReset={handleReset}
+        handleFilter={(val) => handleFilter(val)}
+      />
       <Pagination
         currentPage={currentPage}
         numberOfPages={numberOfPages}
